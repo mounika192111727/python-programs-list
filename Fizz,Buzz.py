467f40c82ff2a18140c9fc88433671ff64ef9fe3
mounika192111727 <113505434+mounika192111727@users.noreply.github.com>
2022-10-07 09:37:56 +0530

    Add files via upload

---
+++ Fizz,Buzz.py
@@ -0,0 +1,15 @@
+for fizzbuzz in range(1,100): 
+
+    if fizzbuzz % 15 == 0: 
+
+        print("FizzBuzz")                                         
+
+    elif fizzbuzz % 3 == 0:     
+
+        print("Fizz")                                         
+
+    elif fizzbuzz % 5 == 0:         
+
+        print("Buzz")                                     
+
+    print(fizzbuzz)
